9477417647208643388ff6340c72bf02595a8270
GabrielKoba <koberagabriel@gmail.com>
2020-07-10 17:06:05 -0300

    Player Movement Added

---
+++ Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController2D : MonoBehaviour {
+
+	private Rigidbody2D m_rigidbody2D;
+	[Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;	// How much to smooth out the movement
+	private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+	private Vector3 m_Velocity = Vector3.zero;
+
+	private void Awake() {
+		m_rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+	public void Move(Vector2 move) {
+        // Move the character by finding the target velocity
+		Vector3 targetVelocity = new Vector2(move.x * 10, move.y * 10);
+		// And then smoothing it out and applying it to the character
+		m_rigidbody2D.velocity = Vector3.SmoothDamp(m_rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+
+		// If the input is moving the player right and the player is facing left...
+		if (move.x > 0 && !m_FacingRight) {
+			// ... flip the player.
+			Flip();
+		}
+		// Otherwise if the input is moving the player left and the player is facing right...
+		else if (move.x < 0 && m_FacingRight) {
+			// ... flip the player.
+			Flip();
+		}
+    }
+    
+	private void Flip()
+	{
+		// Switch the way the player is labelled as facing.
+		m_FacingRight = !m_FacingRight;
+
+		// Multiply the player's x local scale by -1.
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField] private CharacterController2D m_controller;
+    [SerializeField] private Rigidbody2D m_rigidbody2D;
+
+    [SerializeField] float playerMoveSpeed;
+    private float m_horizontalMove = 0f;
+    private float m_verticalMove = 0f;
+
+    private Vector2 moveClamped;
+
+	private void Awake() {
+        m_controller = GetComponent<CharacterController2D>();
+		m_rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        m_horizontalMove = Input.GetAxisRaw("Horizontal") * playerMoveSpeed;
+        m_verticalMove = Input.GetAxisRaw("Vertical") * playerMoveSpeed;
+
+        moveClamped = new Vector2(m_horizontalMove, m_verticalMove);
+        moveClamped = Vector2.ClampMagnitude(moveClamped, playerMoveSpeed);
+    }
+
+    void FixedUpdate(){
+        m_controller.Move(moveClamped * Time.fixedDeltaTime);
+    }
+}
